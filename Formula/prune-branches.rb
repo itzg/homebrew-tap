d75fc4d09b65489de01c89353c0fa02156616661
jreleaserbot <jreleaser@kordamp.org>
2021-12-05 02:59:44 +0000

    prune-branches v1.2.7

--- Formula/prune-branches.rb
+++ Formula/prune-branches.rb
@@ -1,9 +1,9 @@
 class PruneBranches < Formula
   desc "Prunes local git branches without remote branch"
   homepage "https://github.com/itzg/prune-branches"
-  version "1.2.0"
-  url "https://github.com/itzg/prune-branches/releases/download/v1.2.0/prune-branches-1.2.0.zip"
-  sha256 "844d608c285e47fd10175a3757b6edbc3a4a626eb6425e9fdda47d3e0f618aff"
+  url "https://github.com/itzg/prune-branches/releases/download/v1.2.7/prune-branches-1.2.7.zip"
+  version "1.2.7"
+  sha256 "3e40a5880abd16ee6b313955227f68c56db36ba19df1dcba759bbf411c35e2e5"
   license "MIT"
 
   depends_on "openjdk@17"
@@ -15,6 +15,6 @@ class PruneBranches < Formula
 
   test do
     output = shell_output("#{bin}/prune-branches --version")
-    assert_match "1.2.0", output
+    assert_match "1.2.7", output
   end
 end
