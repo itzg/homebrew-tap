4528af4f81472325f2731e4ad6581b91ced34ffd
jreleaserbot <jreleaser@kordamp.org>
2022-01-25 03:23:29 +0000

    mc-image-helper v1.16.1

--- Formula/mc-image-helper.rb
+++ Formula/mc-image-helper.rb
@@ -1,9 +1,9 @@
 class McImageHelper < Formula
   desc "mc-image-helper"
   homepage "https://github.com/itzg/mc-image-helper"
-  url "https://github.com/itzg/mc-image-helper/releases/download/v1.16.0/mc-image-helper-1.16.0.zip"
-  version "1.16.0"
-  sha256 "d6de339c37807854e5bc00d8fc353bbd36f2b11f759d4163102677680068d9a0"
+  url "https://github.com/itzg/mc-image-helper/releases/download/v1.16.1/mc-image-helper-1.16.1.zip"
+  version "1.16.1"
+  sha256 "6d697b44017491a74a2709e8cf893e9f2c87b97369be1673ac05a2f359e6acf9"
   license "MIT"
 
   depends_on "openjdk@8"
@@ -15,6 +15,6 @@ class McImageHelper < Formula
 
   test do
     output = shell_output("#{bin}/mc-image-helper --version")
-    assert_match "1.16.0", output
+    assert_match "1.16.1", output
   end
 end
