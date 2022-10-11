e38fab4a9f6848b0595dd8edd0f56e9884ffbe09
Geoff Bourne <itzgeoff@gmail.com>
2022-10-10 21:02:16 -0500

    github-releaser: prune-branches 1.3.1

--- Formula/prune-branches.rb
+++ Formula/prune-branches.rb
@@ -1,20 +1,15 @@
+
 class PruneBranches < Formula
   desc "Prunes local git branches without remote branch"
   homepage "https://github.com/itzg/prune-branches"
-  url "https://github.com/itzg/prune-branches/releases/download/v1.3.0/prune-branches-1.3.0.zip"
-  version "1.3.0"
-  sha256 "3b309c440dc6b9d4c54621d202183c6165e4ee39fc7eb743baa2f0895e600077"
+  url "https://github.com/itzg/prune-branches/releases/download/1.3.1/prune-branches-1.3.1.tgz"
+  sha256 "4f7ff3c88634d84d79d32739566c4f8d8aa682ed5f85631afe51268dd86c3c26"
   license "MIT"
 
-  depends_on "openjdk@17"
+  depends_on "java"
 
   def install
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/prune-branches"
-  end
-
-  test do
-    output = shell_output("#{bin}/prune-branches --version")
-    assert_match "1.3.0", output
   end
 end
