9f6114afb9c3a8a4c2284cab55308cd005060d7b
Geoff Bourne <itzgeoff@gmail.com>
2022-10-23 16:22:54 -0500

    prune-branches: released 1.3.5

--- Formula/prune-branches.rb
+++ Formula/prune-branches.rb
@@ -1,9 +1,8 @@
-
 class PruneBranches < Formula
   desc "Prunes local git branches without remote branch"
   homepage "https://github.com/itzg/prune-branches"
-  url "https://github.com/itzg/prune-branches/releases/download/1.3.4/prune-branches-1.3.4.tgz"
-  sha256 "f9ff71017fe7015e4265da789d1a99f4e7c436f8c4a65221578d73ef9925e583"
+  url "https://github.com/itzg/prune-branches/releases/download/1.3.5/prune-branches-1.3.5.tgz"
+  sha256 "0ea411aa51e083bb3c0008ee6180d672947e47c44726d28b65dd4ddf72bfb9a6"
   license "MIT"
 
   depends_on "java"
