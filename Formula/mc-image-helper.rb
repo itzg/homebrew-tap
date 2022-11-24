4fb02dc0d9f3e88ac3bf521c7b72b3c963e02039
Geoff Bourne <itzgeoff@gmail.com>
2022-11-23 21:29:31 -0600

    mc-image-helper: released 1.22.9

--- Formula/mc-image-helper.rb
+++ Formula/mc-image-helper.rb
@@ -1,20 +1,14 @@
 class McImageHelper < Formula
   desc "mc-image-helper"
   homepage "https://github.com/itzg/mc-image-helper"
-  url "https://github.com/itzg/mc-image-helper/releases/download/v1.16.11/mc-image-helper-1.16.11.zip"
-  version "1.16.11"
-  sha256 "575781b63d50311b19cd63ef041ff5482e5accedab3836bd5722b75deef0ade4"
+  url "https://github.com/itzg/mc-image-helper/releases/download/1.22.9/mc-image-helper-1.22.9.tgz"
+  sha256 "7979786c846b2276167da2ee51fc8fe107ff2e375f3810a589d2c79757f6ef7c"
   license "MIT"
 
-  depends_on "openjdk@8"
+  depends_on "java"
 
   def install
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/mc-image-helper"
-  end
-
-  test do
-    output = shell_output("#{bin}/mc-image-helper --version")
-    assert_match "1.16.11", output
   end
 end
