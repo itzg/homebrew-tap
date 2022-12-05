abe66fe138d48bb3f13e078112843d5f8b211c69
Geoff Bourne <itzgeoff@gmail.com>
2022-12-04 21:52:18 -0600

    mc-image-helper: released 1.22.10

--- Formula/mc-image-helper.rb
+++ Formula/mc-image-helper.rb
@@ -1,8 +1,8 @@
 class McImageHelper < Formula
   desc "mc-image-helper"
   homepage "https://github.com/itzg/mc-image-helper"
-  url "https://github.com/itzg/mc-image-helper/releases/download/1.22.9/mc-image-helper-1.22.9.tgz"
-  sha256 "7979786c846b2276167da2ee51fc8fe107ff2e375f3810a589d2c79757f6ef7c"
+  url "https://github.com/itzg/mc-image-helper/releases/download/1.22.10/mc-image-helper-1.22.10.tgz"
+  sha256 "44d18140315ebad9b7c5e66cc1aec04ee9dded055aa3457025e49c14128283f7"
   license "MIT"
 
   depends_on "java"
