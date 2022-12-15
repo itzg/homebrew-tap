a6d39d05db09514f2a03d04f9579887fbefadf62
Geoff Bourne <itzgeoff@gmail.com>
2022-12-14 22:34:14 -0600

    file-tree-as-json: released 1.0-SNAPSHOT

--- Formula/file-tree-as-json.rb
+++ Formula/file-tree-as-json.rb
@@ -1,8 +1,8 @@
 class FileTreeAsJson < Formula
   desc ""
   homepage ""
-  url "https://github.com/itzg/file-tree-as-json/releases/download/1.0.0/file-tree-as-json-1.0-SNAPSHOT.tar"
-  sha256 "b5fbfdf60cac6a0415d2b818a0b9afcd9857f0cad31551c4a5c60aff0b0aace3"
+  url "https://github.com/itzg/file-tree-as-json/releases/download/1.0.1/file-tree-as-json-1.0-SNAPSHOT.tar"
+  sha256 "89f794d11fcb6af62cf788723d6448a826295ba89b3410c1d1230c96cfe833f9"
   license ""
 
   depends_on "java"
