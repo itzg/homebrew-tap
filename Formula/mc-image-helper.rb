71be713ab62fa81e97d564e194048bab9b4a664d
Geoff Bourne <itzgeoff@gmail.com>
2023-04-08 15:13:58 -0500

    mc-image-helper: released 1.27.3

--- Formula/mc-image-helper.rb
+++ Formula/mc-image-helper.rb
@@ -1,8 +1,8 @@
 class McImageHelper < Formula
   desc "This tool does the complicated bits for itzg/minecraft-server"
   homepage "https://github.com/itzg/mc-image-helper"
-  url "https://github.com/itzg/mc-image-helper/releases/download/1.27.2/mc-image-helper-1.27.2.tgz"
-  sha256 "ec0158744a6d7911b3cc93b4720a570d06a9c695e9fb7040a93cfc78d4258df4"
+  url "https://github.com/itzg/mc-image-helper/releases/download/1.27.3/mc-image-helper-1.27.3.tgz"
+  sha256 "54d79fe5882cce3b98992c350576502b3edb1df756f128f90fce8d837aad2d13"
   license "MIT"
 
   depends_on "java"
